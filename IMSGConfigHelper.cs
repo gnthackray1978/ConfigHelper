a30a9f05c72afe14837f28ec38872f3f661ec70f
George <gnthackray@btinternet.com>
2021-04-12 19:06:46 +0100

    initial commit

---
+++ IMSGConfigHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace ConfigHelper
+{
+    public interface IMSGConfigHelper
+    {
+        string TestClientUrl { get; set; }
+        string AuthServerUrl { get; set; }
+        string MSGApiGenUrl { get; set; }
+
+        string MSGGenDB01 { get; set; }
+
+    }
+
+
+}
